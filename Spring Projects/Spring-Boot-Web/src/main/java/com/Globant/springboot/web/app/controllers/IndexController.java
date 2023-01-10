4d98b2e7f355916c5792df676e8a5c0da5d0a157
Manuel Amores Gonz <amoresgonzalezmanuel@gmail.com>
2023-01-10 15:38:06 -0600

    Creando la vista de usuarios

--- Spring Projects/Spring-Boot-Web/src/main/java/com/Globant/springboot/web/app/controllers/IndexController.java
+++ Spring Projects/Spring-Boot-Web/src/main/java/com/Globant/springboot/web/app/controllers/IndexController.java
@@ -1,11 +1,17 @@
 package com.Globant.springboot.web.app.controllers;
 
+import com.Globant.springboot.web.app.Models.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
 	@GetMapping({"/index","/","/home"})
@@ -20,7 +26,7 @@ public class IndexController {
 	}*/
 //Enviar parametros pero a vista elegida
 	public ModelAndView index (ModelAndView mv){
-		String prueba = "Hol";
+		String prueba = "Hola";
 		if(prueba.equals("Hola")){
 			mv.addObject("titulo", "Hola Spring Framework de html prueba");
 			mv.setViewName("prueba");
@@ -29,5 +35,25 @@ public class IndexController {
 		mv.addObject("titulo", "Hola Spring Framework de index");
 		mv.setViewName("index");
 		return mv;
+	}
+
+
+	@RequestMapping("/perfil")
+	public String perfil(Model model){
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Manuel");
+		usuario.setApellido("Amores");
+		usuario.setEmail("manuel@gmail.com");
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+		return "perfil";
+	}
+
+	@RequestMapping("/listar")
+	public String listar(Model model){
+		List<Usuario> usuarios = new ArrayList<>();
+		model.addAttribute("titulo", "Listado de usuarios");
+		model.addAttribute("usuarios", usuarios);
+		return "listar";
 	}
 }

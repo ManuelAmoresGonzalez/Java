ce1584a2e7f75df4f2821a824572cc30a0b4d7c9
Manuel Enrique Amores Gonzalez <manuel.amores@globant.com>
2023-02-08 11:52:06 -0600

    Properties and other functions

--- Spring Projects/Spring-Boot-Web/src/main/java/com/Globant/springboot/web/app/controllers/IndexController.java
+++ Spring Projects/Spring-Boot-Web/src/main/java/com/Globant/springboot/web/app/controllers/IndexController.java
@@ -1,38 +1,40 @@
 package com.Globant.springboot.web.app.controllers;
 
 import com.Globant.springboot.web.app.Models.Usuario;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
 @RequestMapping("/app")
 public class IndexController {
 
-	@GetMapping({"/index","/","/home"})
-	/*public String index() {
-		return "index";
-	}*/
+@Value("${texto.indexController.index.titulo}")
+private String textoIndex;
 
-	//Formas para enviar por parametros
-	/*public String index(Model model) {
-		model.addAttribute("titulo", "Hola Spring Framework");
-		return "index";
-	}*/
+@Value("${texto.indexController.perfil.titulo}")
+private String textoPerfil;
+
+@Value("${texto.indexController.listar.titulo}")
+private String textoListar;
 //Enviar parametros pero a vista elegida
+	@RequestMapping("/index")
 	public ModelAndView index (ModelAndView mv){
-		String prueba = "Hola";
+		String prueba = "Hol";
 		if(prueba.equals("Hola")){
 			mv.addObject("titulo", "Hola Spring Framework de html prueba");
 			mv.setViewName("prueba");
 			return mv;
 		}
-		mv.addObject("titulo", "Hola Spring Framework de index");
+		mv.addObject("titulo", textoIndex);
 		mv.setViewName("index");
 		return mv;
 	}
@@ -45,15 +47,32 @@ public class IndexController {
 		usuario.setApellido("Amores");
 		usuario.setEmail("manuel@gmail.com");
 		model.addAttribute("usuario", usuario);
-		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+		model.addAttribute("titulo", textoPerfil.concat(usuario.getNombre()));
 		return "perfil";
 	}
 
 	@RequestMapping("/listar")
 	public String listar(Model model){
-		List<Usuario> usuarios = new ArrayList<>();
-		model.addAttribute("titulo", "Listado de usuarios");
-		model.addAttribute("usuarios", usuarios);
+		/*List<Usuario> usuarios = Arrays.asList(
+				new Usuario("Mario","Amores","manolo@gmail.com"),
+				new Usuario("Carolina","Angelina","marinila@gmail.com"),
+				new Usuario("Manuel","Amores","manuel@gmail.com"),
+				new Usuario("Carlitos","Carlino","carl@gmail.com")
+		);*/
+		model.addAttribute("titulo", textoListar);
+		//model.addAttribute("usuarios", usuarios);
 		return "listar";
+	}
+
+	@ModelAttribute("usuarios")
+	public List<Usuario> poblarUsuarios(){
+
+		List<Usuario> usuarios = Arrays.asList(
+				new Usuario("Mario","Amores","manolo@gmail.com"),
+				new Usuario("Carolina","Angelina","marinila@gmail.com"),
+				new Usuario("Manuel","Amores","manuel@gmail.com"),
+				new Usuario("Carlitos","Carlino","carl@gmail.com")
+		);
+		return usuarios;
 	}
 }

8a04c9b5b71e2d2dda0746a98221e7039dfcd1ef
Manuel Amores González <amoresgonzalezmanuel@gmail.com>
2023-04-19 15:59:36 -0600

    Finish project anonymous classes

--- AnonymousClasses/src/main/java/org/example/Main.java
+++ AnonymousClasses/src/main/java/org/example/Main.java
@@ -2,6 +2,21 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        new Vehiculo(){
+            private int numeroPasajeros;
+
+            public void conducir(){
+                System.out.println("Estoy conduciendo");
+            }
+
+        }.conducir();
+
+        new InterfaceUSuario(){
+
+            public void crearUsuario(){
+                System.out.println("Creando usuario");
+            }
+        }.crearUsuario();
     }
 }

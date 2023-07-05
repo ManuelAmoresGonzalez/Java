248d322a3f84c5774f01792ae26c060ad76347f1
Manuel Amores González <amoresgonzalezmanuel@gmail.com>
2023-07-05 15:22:42 -0600

    Create project

---
+++ CrudSpringBoot/src/test/java/com/globant/CrudSpringBoot/CrudSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globant.CrudSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

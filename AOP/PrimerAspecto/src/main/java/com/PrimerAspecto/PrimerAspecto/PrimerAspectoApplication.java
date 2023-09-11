f653b2940663e16c88de4d01a7957d7c1a353408
manuel-amores <manuel.amores@globant.com>
2023-09-11 14:52:12 -0600

    First project my first aspect

---
+++ AOP/PrimerAspecto/src/main/java/com/PrimerAspecto/PrimerAspecto/PrimerAspectoApplication.java
@@ -0,0 +1,13 @@
+package com.PrimerAspecto.PrimerAspecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerAspectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerAspectoApplication.class, args);
+	}
+
+}
---
+++ AOP/PrimerAspecto/src/test/java/com/PrimerAspecto/PrimerAspecto/PrimerAspectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PrimerAspecto.PrimerAspecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerAspectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

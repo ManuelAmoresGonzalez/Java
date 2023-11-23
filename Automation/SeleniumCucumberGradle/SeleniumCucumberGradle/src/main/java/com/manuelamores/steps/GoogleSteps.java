58ce07431580d603ed2cd15f65ec766f309d2e48
Manuel Enrique Amores Gonzalez <manuel.amores@globant.com>
2023-11-23 15:49:27 -0600

    Creating feature to google search cucumber test

---
+++ Automation/SeleniumCucumberGradle/SeleniumCucumberGradle/src/main/java/com/manuelamores/steps/GoogleSteps.java
@@ -0,0 +1,24 @@
+package com.manuelamores.steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GoogleSteps {
+    @Given("^I am on the google search page$")
+    public void iAmOnTheGoogleSearchPage() {
+    }
+
+    @When("^I enter a search criteria$")
+    public void iEnterASearchCriteria() {
+    }
+
+    @And("^Click on the search button$")
+    public void clickOnTheSearchButton() {
+    }
+
+    @Then("^The results match the criteria$")
+    public void theResultsMatchTheCriteria() {
+    }
+}
